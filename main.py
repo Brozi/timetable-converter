13b3bff8a8b01cd60b1322b68384df14ff6def20
Brozi <jasiubar@gmail.com>
2026-02-04 23:35:36 +0100

    fix

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ LOGICAL_ORDER = [
     'Pojemność'
 ]
 
-# --- PRZYJAZNE NAZWY DLA MENU WYBORU (tylko wyświetlanie) ---
+# --- PRZYJAZNE NAZWY DLA MENU WYBORU ---
 FRIENDLY_NAME_MAP = {
     'Tytuł': 'Nazwa przedmiotu',
     'Ogłoszony początek': 'Od (godzina)',
